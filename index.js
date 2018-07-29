9d4cae785379cde1679e585b945bda5ef87ce4d7
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 15:08:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ $(document).ready(function (){
 });
 
 function searchRepositories(){
-  const terms=$('#searchTerms').val();
+  const terms=$('#searchTerms').val().replace(" ","+");
   console.log(terms);
 }
