ddbd83887ca92d25d8ec7fe2bb787b4695917b86
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 18:29:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function searchRepositories(){
     $("#results").html(resp.items.map(i=>{
       return `
       <div>
-        <h2><a href="${i.html_url}">${i.name}</a></h2>
+        <h4><a href="${i.html_url}">${i.name}</a></h4>
         <p>${i.description}</p>
         <h5>Created By: <a href="${i.owner.url}">${i.owner.login}</a></h5>
         <a href="#" onclick="getCommits(this)" data-username="${i.owner.login}" data-repository="${i.name}">Get Commits</a>
