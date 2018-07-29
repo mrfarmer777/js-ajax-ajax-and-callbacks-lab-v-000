7295deeac4520fd01e0882a04eb8a27f6bb2e895
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 15:14:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function searchRepositories(){
   const query="https://api.github.com/search/repositories?q="+terms;
   $.get(query,function(response){
     
-    $("#results").html(response);
+    $("#results").html(response).fail(function(error){
+      console.log(error);
+    });
   });
 }
