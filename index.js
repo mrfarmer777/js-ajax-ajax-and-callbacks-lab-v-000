d43c0b20f20c8deac5da904e6c492b69bc1f5ea8
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 15:12:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ $(document).ready(function (){
 function searchRepositories(){
   const terms=$('#searchTerms').val().replace(" ","+");
   const query="https://api.github.com/search/repositories?q="+terms;
-  $.get(query,)
+  $.get(query,function(response){
+    
+    $()
+  })
 }
